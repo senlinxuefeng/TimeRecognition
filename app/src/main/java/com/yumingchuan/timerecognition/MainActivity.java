73619067567e7e80f7c5c8cc0ac32ec7a9ec2071
senlinxuefeng <1107963848@qq.com>
2016-12-13 16:53:22 +0800

    首次提交

---
+++ app/src/main/java/com/yumingchuan/timerecognition/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yumingchuan.timerecognition;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context =this;
+        new TimeAnalyseTest().test(context);
+    }
+}
